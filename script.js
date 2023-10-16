404ba88632232fd757fa6483729bcb54c5a003d8
Rashmitha79 <134279042+Rashmitha79@users.noreply.github.com>
2023-10-16 12:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
-//your code here
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sorted Band Names</title>
-</head>
-<body>
+// Suppose an array that contains a list of band names
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Anthrax'];
 
-  <ul id="band"></ul>
+// Function to sort band names and display them in the 'band' ul
+function sortAndDisplayBandNames(bandNames) {
+  // Remove articles and convert to lowercase for sorting
+  const sortedBandNames = bandNames
+    .map(name => name.replace(/^(a|an|the)\s+/i, '').toLowerCase())
+    .sort();
 
-  <script>
-    // Function to sort band names
-    function sortBandNames(bandNames) {
-      // Remove articles and convert to lowercase for sorting
-      const sortedBandNames = bandNames
-        .map(name => name.replace(/^(a|an|the)\s+/i, '').toLowerCase())
-        .sort();
+  // Get the 'band' ul element
+  const bandList = document.getElementById('band');
 
-      // Get the ul element
-      const bandList = document.getElementById('band');
+  // Clear existing content
+  bandList.innerHTML = '';
 
-      // Create li elements and append them to the ul
-      sortedBandNames.forEach(name => {
-        const li = document.createElement('li');
-        li.textContent = name;
-        bandList.appendChild(li);
-      });
-    }
+  // Create li elements and append them to the 'band' ul
+  sortedBandNames.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    bandList.appendChild(li);
+  });
+}
 
-    // Example usage
-    const bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Anthrax'];
-    sortBandNames(bandNames);
-  </script>
-
-</body>
-</html>
-
-
+// Example usage
+sortAndDisplayBandNames(bandNames);
